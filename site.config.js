59db1bc1660ab6738d660e866d7d1d4f4a3c62c8
thekanon <dlengjs123@gmail.com>
2023-10-10 02:00:10 +0900

    프로파일 초기화

--- site.config.js
+++ site.config.js
@@ -1,32 +1,35 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "dooveloper",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "FE개발자입니다. 클라이언트 관점에서 데이터 시각화와 SEO에 관심이 많습니다.",
+    email: "hiphopbikedev@kakao.com",
+    linkedin: "",
+    github: "thekanon",
     instagram: "",
   },
   projects: [
     {
       name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      href: "https://github.com/thekanon/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "dooveloper-log",
+    description: "welcome to dooveloper-log!",
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  seo: {
+    keywords: ["blog", "notion", "nextjs", "vercel", "react"],
+  },
 
   // notion configuration (required)
   notionConfig: {
@@ -47,7 +50,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",

8aa61396915a13e90fdd4fcf06ca16bfcdcba201
thekanon <dlengjs123@gmail.com>
2023-10-18 14:32:04 +0900

    사이트맵 설정 변경

--- site.config.js
+++ site.config.js
@@ -12,7 +12,7 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `doovelop-log`,
       href: "https://github.com/thekanon/morethan-log",
     },
   ],
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://doovelop-log.vercel.app",
   since: 2023, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
